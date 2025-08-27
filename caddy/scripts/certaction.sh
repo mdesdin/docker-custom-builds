6c891405fff7eba26720107d46e353d0b7dc0840
mdesdin <mdesdin@gmail.com>
2025-08-27 09:48:56 +0200

    Additional improvements
    Replaced HOSTNAME with HOST_NAME
    Fixed permissions for postgres

--- caddy/scripts/certaction.sh
+++ caddy/scripts/certaction.sh
@@ -35,10 +35,13 @@ for domain in $ACTION_DOMAINS; do
   if [ "$domain" = "$IDENTIFIER" ]; then
     echo "Identifier $IDENTIFIER matches $domain in ACTION_DOMAINS"
     #echo "Reloading PostgreSQL config..."
-    #curl -H "Content-Type: application/json" -d "{\"username\": \"caddy@$HOSTNAME\", \"content\": \"Reloading PostgreSQL config...\"}" $DISCORD_WEBHOOK
+    #chmod g+rx $DEST_DIR
+    #chmod g+r $DEST_DIR/$IDENTIFIER.*
+    #chgrp -R postgres $DEST_DIR
+    #curl -H "Content-Type: application/json" -d "{\"username\": \"caddy@$HOST_NAME\", \"content\": \"Reloading PostgreSQL config...\"}" $DISCORD_WEBHOOK
     #psql postgresql://postgres:${POSTGRES_PASSWORD}@postgresql -c "SELECT pg_reload_conf();"
     #echo "Reloading Stalwart certificates..."
-    #curl -H "Content-Type: application/json" -d "{\"username\": \"caddy@$HOSTNAME\", \"content\": \"Reloading Stalwart certificates...\"}" $DISCORD_WEBHOOK
+    #curl -H "Content-Type: application/json" -d "{\"username\": \"caddy@$HOST_NAME\", \"content\": \"Reloading Stalwart certificates...\"}" $DISCORD_WEBHOOK
     #stalwart-cli -u http://stalwart:8080 -c $STW_CREDENTIALS server reload-certificates
     break
   fi
